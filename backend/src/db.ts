76a87f723d72ebd55d354628d5e30257934fefd1
Bumgu Kang <flrhyme9@gmail.com>
2025-07-31 21:02:12 +0900

    add: typeorm

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,15 +1,36 @@
-import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
+//import mysql from 'mysql2/promise';
+//import dotenv from 'dotenv';
+//
+//dotenv.config();
+//
+//export const pool = mysql.createPool({
+//    host: process.env.DATABASE_HOST,
+//    user: process.env.DATABASE_USER,
+//    password: process.env.DATABASE_PASSWORD,
+//    database: process.env.DATABASE_NAME,
+//    port: process.env.DATABASE_PORT ? Number(process.env.DATABASE_PORT) : 3306,
+//    waitForConnections: true,
+//    connectionLimit: 10,
+//    queueLimit: 0,
+//});
+//
+import { DataSource } from "typeorm";
 
-dotenv.config();
-
-export const pool = mysql.createPool({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    port: process.env.DATABASE_PORT ? Number(process.env.DATABASE_PORT) : 3306,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+const AppDataSource = new DataSource({
+  type: "mysql",
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT ? parseInt(process.env.DATABASE_PORT, 10) : undefined,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME
 });
+
+(async () => {
+  try {
+    await AppDataSource.initialize();
+    console.log("Data source has been initialized!");
+  } catch (err) {
+    console.error("Error during Datasource initialization", err);
+  }
+})();
+

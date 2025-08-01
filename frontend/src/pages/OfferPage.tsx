9d90b9d82a3d23706b800930944892e9d956659d
khj980211 <khj980211@naver.com>
2025-08-01 10:23:26 +0900

    offer page state added

--- frontend/src/pages/OfferPage.tsx
+++ frontend/src/pages/OfferPage.tsx
@@ -7,13 +7,22 @@ const OfferPage: React.FC = () => {
   const [activeTab, setActiveTab] = useState<"region" | "model">("region");
 
   const [regions, setRegions] = useState<Region[]>([]);
-  const [selectedRegion, setSelectedRegion] = useState<Region | null>(null);
   const [subRegions, setSubRegions] = useState<Region[]>([]);
-  const [selectedSubRegions, setSelectedSubRegions] = useState<
-    RegionWithParent[]
-  >([]);
+  const [selectedRegions, setSelectedRegions] = useState<Region | null>(null);
+  const [selectedSubRegions, setSelectedSubRegions] = useState<RegionWithParent[]>([]);
 
-  const [selectedModel, setSelectedModel] = useState("all");
+  const [brands, setBrands] = useState<string[]>([]);
+  const [models, setModels] = useState<string[]>([]);
+  const [storages, setStorages] = useState<string[]>([]);
+  const [selectedBrands, setSelectedBrands] = useState<string[]>([]);
+  const [selectedModels, setSelectedModels] = useState<string[]>([]);
+  const [selectedStorages, setSelectedStorages] = useState<string[]>([]);
+
+  const [carriers, setCarriers] = useState<string[]>([]);
+  const [buyingTypes, setBuyingTypes] = useState<string[]>([]);
+  const [selectedCarriers, setSelectedCarriers] = useState<string[]>([]);
+  const [selectedBuyingTypes, setSelectedBuyingTypes] = useState<string[]>([]);
+
 
   const SERVER = import.meta.env.VITE_API_URL;
 
@@ -26,8 +35,8 @@ const OfferPage: React.FC = () => {
 
   // 시/도 지역 선택 시 구/군(하위 지역) 지역 데이터 GET
   useEffect(() => {
-    if (selectedRegion !== null) {
-      const parentId = selectedRegion?.region_id;
+    if (selectedRegions !== null) {
+      const parentId = selectedRegions?.region_id;
       fetch(`${SERVER}/api/offer/regions?parentId=${parentId}`)
         .then((res) => res.json())
         .then((data) => {
@@ -39,17 +48,17 @@ const OfferPage: React.FC = () => {
           setSubRegions([all, ...data]);
         });
     }
-  }, [selectedRegion, SERVER]);
+  }, [selectedRegions, SERVER]);
 
   const handleRegionChange = (selectedRegion: Region) => {
-    setSelectedRegion(selectedRegion);
+    setSelectedRegions(selectedRegion);
   };
 
   const handleSubRegionChange = (child: Region) => {
-    if (!selectedRegion) return;
+    if (!selectedRegions) return;
 
     setSelectedSubRegions((prev) => {
-      const parentId = selectedRegion.region_id;
+      const parentId = selectedRegions.region_id;
 
       const isAllSelected = child.region_id === -parentId;
 
@@ -58,7 +67,7 @@ const OfferPage: React.FC = () => {
         const filtered = prev.filter(
           (item) => item.parent.region_id !== parentId
         );
-        return [...filtered, { parent: selectedRegion, child }];
+        return [...filtered, { parent: selectedRegions, child }];
       } else {
         const filtered = prev.filter(
           (item) =>
@@ -79,7 +88,7 @@ const OfferPage: React.FC = () => {
           );
         } else {
           // 새로운 항목 추가
-          return [...filtered, { parent: selectedRegion, child }];
+          return [...filtered, { parent: selectedRegions, child }];
         }
       }
     });
@@ -132,7 +141,7 @@ const OfferPage: React.FC = () => {
                       >
                         <CustomCheckbox
                           label={region.name}
-                          checked={selectedRegion?.name === region.name}
+                          checked={selectedRegions?.name === region.name}
                           onChange={() => handleRegionChange(region)}
                         />
                       </label>
@@ -141,7 +150,7 @@ const OfferPage: React.FC = () => {
                 </div>
                 <div className="w-3/4 max-h-60 overflow-y-auto border border-gray-300 dark:border-gray-400 rounded-lg p-3 bg-white dark:bg-[#292929]">
                   <div className="grid grid-cols-2 md:grid-cols-5 gap-2">
-                    {selectedRegion === null ? (
+                    {selectedRegions === null ? (
                       <div className="text-gray-400 text-xs">
                         시/도를 먼저 선택하세요.
                       </div>
@@ -173,7 +182,7 @@ const OfferPage: React.FC = () => {
                 <label className="block text-sm font-medium mb-2 text-foreground-light dark:text-foreground-dark">
                   모델 선택
                 </label>
-                <select
+                {/* <select
                   value={selectedModel}
                   onChange={(e) => setSelectedModel(e.target.value)}
                   className="w-full px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-lg bg-white dark:bg-gray-700 text-foreground-light dark:text-foreground-dark focus:ring-2 focus:ring-primary-light dark:focus:ring-primary-dark focus:border-transparent"
@@ -184,7 +193,7 @@ const OfferPage: React.FC = () => {
                   <option value="galaxy-z-flip5">갤럭시 Z 플립5</option>
                   <option value="iphone-14">아이폰 14</option>
                   <option value="galaxy-s23">갤럭시 S23</option>
-                </select>
+                </select> */}
               </div>
             )}
           </div>
